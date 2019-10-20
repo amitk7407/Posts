8c46a4aee49ab15700fd884cff22137eb2548e16
Amit <amit.kumar@metacube.com>
2019-10-20 12:52:37 +0530

    deployment fixes to portal

---
+++ post-ui/build/precache-manifest.875f3b9fc36c278fa4f99521f3806df4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e91e5cbb8169bd778a8c50f350e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef476cdb081afc6a258",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "6edb88818f0110a5b520",
+    "url": "/static/css/main.e8a2a4f9.chunk.css"
+  },
+  {
+    "revision": "9ef476cdb081afc6a258",
+    "url": "/static/js/2.d3e2c908.chunk.js"
+  },
+  {
+    "revision": "6edb88818f0110a5b520",
+    "url": "/static/js/main.1c1a7c17.chunk.js"
+  },
+  {
+    "revision": "6f85bb3d91196f82a1a7",
+    "url": "/static/js/runtime-main.bfa4fd68.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
